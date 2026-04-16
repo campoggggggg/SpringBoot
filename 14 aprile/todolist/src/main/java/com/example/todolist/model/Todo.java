4f7c1438e343eed5a95243fcb8dd99178cf57248
Gabriele <gabriele.campomizzi@gmail.com>
2026-04-16 08:44:11 +0200

    .

--- 14 aprile/todolist/src/main/java/com/example/todolist/model/Todo.java
+++ 14 aprile/todolist/src/main/java/com/example/todolist/model/Todo.java
@@ -3,7 +3,7 @@ package com.example.todolist.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,7 +18,10 @@ public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "descrizione", nullable = false)
     private String descrizione;
+    
     private boolean completato;
 
 }
